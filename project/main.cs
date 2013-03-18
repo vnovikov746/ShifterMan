3eee230a060b42108654c8a43ef15b2c9e6078de
koby vurgaft <koby@koby-ThinkPad-T400.(none)>
2013-03-18 20:52:50 +0200

    first project commit

---
+++ project/main.cs
@@ -0,0 +1,10 @@
+class ShifterManMain {
+	
+	public static void Main() {
+		
+		// placeholder
+		System.Console.WriteLine("Hello ShifterMan");
+		System.Console.WriteLine("press enter to exit..");
+		System.Console.ReadLine();
+	}
+}
