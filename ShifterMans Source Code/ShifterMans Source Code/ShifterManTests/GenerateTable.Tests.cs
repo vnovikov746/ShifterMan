d3a4dc112c699bf08eb605234679f36dc1a4ae24
Vladimir Novikov <kakifish@hotmail.com>
2013-05-27 20:57:35 +0300

    Tests Edited

--- ShifterMans Source Code/ShifterMans Source Code/ShifterManTests/GenerateTable.Tests.cs
+++ ShifterMans Source Code/ShifterMans Source Code/ShifterManTests/GenerateTable.Tests.cs
@@ -18,7 +18,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
             bool result = t1.shiftExists("Sunday", "7", "15");
             //assert
             Assert.AreEqual(result, true);
-
+            
         }
 
         [TestMethod]
@@ -136,7 +136,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
             //assert
             Assert.AreEqual(schedule.GetAllShifts().Count, 1);
-            Assert.AreEqual(schedule.optionExists(option), true);
+            Assert.AreEqual(schedule.optionExists(option),true);
 
         }
 
@@ -200,5 +200,86 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
             Assert.AreEqual(schedule.optionExists(option2), true);
 
         }
+
+        [TestMethod]
+        public void GenerateScheduleComplexTest()
+        {
+            //arrange
+            ShiftTable shifts = new ShiftTable();
+            Shift shift1 = new Shift("", "", "Sunday", "7", "15", "Sion", "", "");
+            Shift shift2 = new Shift("", "", "Sunday", "15", "23", "Sion", "", "");
+            Shift shift3 = new Shift("", "", "Sunday", "23", "7", "Sion", "", "");
+            Shift shift4 = new Shift("", "", "Monday", "7", "15", "Sion", "", "");
+            Shift shift5 = new Shift("", "", "Monday", "15", "23", "Sion", "", "");
+            Shift shift6 = new Shift("", "", "Monday", "23", "7", "Sion", "", "");
+            Shift shift7 = new Shift("", "", "Tuesday", "7", "15", "Sion", "", "");
+            Shift shift8 = new Shift("", "", "Tuesday", "15", "23", "Sion", "", "");
+            Shift shift9 = new Shift("", "", "Tuesday", "23", "7", "Sion", "", "");
+            Shift shift10 = new Shift("", "", "Wednesday", "7", "15", "Sion", "", "");
+            Shift shift11 = new Shift("", "", "Wednesday", "15", "23", "Sion", "", "");
+            Shift shift12 = new Shift("", "", "Wednesday", "23", "7", "Sion", "", "");
+            shifts.AddShift(shift1);
+            shifts.AddShift(shift2);
+            shifts.AddShift(shift3);
+            shifts.AddShift(shift4);
+            shifts.AddShift(shift5);
+            shifts.AddShift(shift6);
+            shifts.AddShift(shift7);
+            shifts.AddShift(shift8);
+            shifts.AddShift(shift9);
+            shifts.AddShift(shift10);
+            shifts.AddShift(shift11);
+            shifts.AddShift(shift12);
+
+            ShiftTable options = new ShiftTable();
+            Shift option1 = new Shift("123456789", "Polishuk", "Sunday", "7", "15", "Sion", "Low", "");
+            Shift option2 = new Shift("123456789", "Polishuk", "Sunday", "15", "23", "Sion", "High", "");
+            Shift option3 = new Shift("123456789", "Polishuk", "Sunday", "23", "7", "Sion", "High", "");
+
+            Shift option4 = new Shift("111222333", "Humi", "Sunday", "7", "15", "Sion", "High", "");
+            Shift option5 = new Shift("111222333", "Humi", "Monday", "7", "15", "Sion", "High", "");
+            Shift option6 = new Shift("111222333", "Humi", "Tuesday", "7", "15", "Sion", "High", "");
+
+            Shift option7 = new Shift("222333444", "Soli", "Wednesday", "7", "23", "Sion", "High", "");
+            Shift option8 = new Shift("222333444", "Soli", "Wednesday", "22", "7", "Sion", "High", "");
+            Shift option9 = new Shift("222333444", "Soli", "Wednesday", "7", "7", "Sion", "High", "");
+
+            Shift option10 = new Shift("333444555", "Kozo", "Wednesday", "7", "15", "Sion", "High", "");
+            Shift option11 = new Shift("333444555", "Kozo", "Wednesday", "15", "23", "Sion", "High", "");
+            Shift option12 = new Shift("333444555", "Kozo", "Wednesday", "23", "7", "Sion", "High", "");
+
+            Shift option13 = new Shift("444555666", "Tkuma", "Monday", "7", "15", "Sion", "High", "");
+            Shift option14 = new Shift("444555666", "Tkuma", "Monday", "15", "23", "Sion", "High", "");
+            options.AddShift(option1);
+            options.AddShift(option2);
+            options.AddShift(option3);
+            options.AddShift(option4);
+            options.AddShift(option5);
+            options.AddShift(option6);
+            //options.AddShift(option7);
+            options.AddShift(option8);
+            //options.AddShift(option9);
+            options.AddShift(option10);
+            options.AddShift(option11);
+            options.AddShift(option12);
+            options.AddShift(option13);
+            options.AddShift(option14);
+
+            //act
+            GenerateTable generator = new GenerateTable(options, shifts);
+            ShiftTable schedule = generator.GenerateSchedule();
+
+            //assert
+            Assert.AreEqual(schedule.GetAllShifts().Count, 7);
+            Assert.AreEqual(schedule.optionExists(option2), true);
+            Assert.AreEqual(schedule.optionExists(option4), true);
+            Assert.AreEqual(schedule.optionExists(option5), true);
+            Assert.AreEqual(schedule.optionExists(option6), true);
+            Assert.AreEqual(schedule.optionExists(option10), true);
+            Assert.AreEqual(schedule.optionExists(option12), true);
+            Assert.AreEqual(schedule.optionExists(option14), true);
+
+        }
+
     }
 //}
